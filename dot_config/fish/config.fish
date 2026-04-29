44c683aa4b866a404e1147efb6110c6f70a49b49
Cleboost <clement.balarot@gmail.com>
2026-04-30 00:17:35 +0200

    style(config): update clear alias to include fastfetch command

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -12,17 +12,11 @@ if status is-interactive
     alias pamcan="pacman"
     alias please="sudo"
 
-    alias gem='nohup kitty --override background_opacity=1.0 gemini >/dev/null 2>&1 &; disown; exit'
-    
-    # Utiliser eza à la place de ls (plus joli avec les icônes)
-    # Assure-toi que le paquet 'eza' est bien installé
     alias ls='eza --icons --group-directories-first'
     alias ll='eza -alF --icons --group-directories-first'
-    
-    # Nettoyage propre du terminal
-    alias clear="printf '\033[2J\033[3J\033[1;1H'"
 
-    # Raccourcis utiles pour Arch
+    alias clear="printf '\033[2J\033[3J\033[1;1H' && fastfetch --config hypr"
+
     alias update="yay"
 end
 export PATH="$HOME/.local/bin:$PATH"
