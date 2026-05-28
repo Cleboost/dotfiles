fba35d939f35628c133039957e796c276ef254e3
Cleboost <clement.balarot@gmail.com>
2026-05-28 19:10:15 +0200

    Update executable_hotspot.sh

--- dot_config/hypr/hyprland/scripts/executable_hotspot.sh
+++ dot_config/hypr/hyprland/scripts/executable_hotspot.sh
@@ -2,5 +2,4 @@
 # Trigger hotspot and suppress output
 /usr/bin/nmcli device wifi hotspot ifname wlan0 ssid cleboost-brain password "coucoubb" > /dev/null 2>&1
 
-# Send the Quickshell notification with Tabler icon name 'wifi'
-/usr/bin/qs -c noctalia-shell ipc call toast send '{"title": "Hotspot", "body": "Hotspot cleboost-brain is now active", "icon": "wifi"}'
+notify-send -a "Wifi" "Hotspot Activated" "Your Wi-Fi hotspot is now active." -i "network-wireless-hotspot-symbolic"
