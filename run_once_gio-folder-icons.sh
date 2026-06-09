4d2a8a84c9103bea4370235b4b91fe86fc9dfa90
Cleboost <clement.balarot@gmail.com>
2026-06-09 09:49:43 +0200

    Add custom Code folder icon and GIO metadata script

--- run_once_gio-folder-icons.sh
+++ run_once_gio-folder-icons.sh
@@ -2,3 +2,4 @@
 # Set special folder icons via GIO metadata (runs once per machine)
 gio set ~/Pictures metadata::custom-icon "file:///usr/share/icons/WhiteSur-dark/places/scalable/folder-pictures.svg"
 gio set ~/Videos metadata::custom-icon "file:///usr/share/icons/WhiteSur-dark/places/scalable/folder-videos.svg"
+gio set ~/Code metadata::custom-icon "file://${HOME}/.local/share/icons/custom/folder-code.svg"
