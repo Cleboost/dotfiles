433cda382afe8406e582adafa1ec433d6b2ec9d0
Cleboost <clement.balarot@gmail.com>
2026-06-09 09:53:38 +0200

    Add custom Projects folder icon

--- run_once_gio-folder-icons.sh
+++ run_once_gio-folder-icons.sh
@@ -3,3 +3,4 @@
 gio set ~/Pictures metadata::custom-icon "file:///usr/share/icons/WhiteSur-dark/places/scalable/folder-pictures.svg"
 gio set ~/Videos metadata::custom-icon "file:///usr/share/icons/WhiteSur-dark/places/scalable/folder-videos.svg"
 gio set ~/Code metadata::custom-icon "file://${HOME}/.local/share/icons/custom/folder-code.svg"
+gio set ~/Projects metadata::custom-icon "file://${HOME}/.local/share/icons/custom/folder-projects.svg"
