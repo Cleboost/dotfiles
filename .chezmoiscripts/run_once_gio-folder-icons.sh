ea27c134264dffa48b9fdac606eb7d89632ddb4b
Cleboost <clement.balarot@gmail.com>
2026-06-18 19:28:27 +0200

    feat(gio): add Documents folder icon to WhiteSur-dark theme and ensure folder exists

--- .chezmoiscripts/run_once_gio-folder-icons.sh
+++ .chezmoiscripts/run_once_gio-folder-icons.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Ensure folders exist first
-mkdir -p ~/Pictures ~/Videos ~/Code ~/Projects
+mkdir -p ~/Pictures ~/Videos ~/Code ~/Projects ~/Documents
 
 gio set ~/Pictures metadata::custom-icon "file:///usr/share/icons/WhiteSur-dark/places/scalable/folder-pictures.svg"
 gio set ~/Videos metadata::custom-icon "file:///usr/share/icons/WhiteSur-dark/places/scalable/folder-videos.svg"
+gio set ~/Documents metadata::custom-icon "file:///usr/share/icons/WhiteSur-dark/places/scalable/folder-documents.svg"
 gio set ~/Code metadata::custom-icon "file://${HOME}/.local/share/icons/custom/folder-code.svg"
 gio set ~/Projects metadata::custom-icon "file://${HOME}/.local/share/icons/custom/folder-projects.svg"
