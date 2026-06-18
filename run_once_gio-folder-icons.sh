1f7916d3c776f416cee474913b5a164f718a27fc
Cleboost <clement.balarot@gmail.com>
2026-06-18 18:52:13 +0200

    fix(gio): ensure target directories exist before running gio set

--- run_once_gio-folder-icons.sh
+++ run_once_gio-folder-icons.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Set special folder icons via GIO metadata (runs once per machine)
+# Ensure folders exist first
+mkdir -p ~/Pictures ~/Videos ~/Code ~/Projects
+
 gio set ~/Pictures metadata::custom-icon "file:///usr/share/icons/WhiteSur-dark/places/scalable/folder-pictures.svg"
 gio set ~/Videos metadata::custom-icon "file:///usr/share/icons/WhiteSur-dark/places/scalable/folder-videos.svg"
 gio set ~/Code metadata::custom-icon "file://${HOME}/.local/share/icons/custom/folder-code.svg"
