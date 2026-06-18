edc10b81859e3cc1d25c1ce0bac7cb035f5d8227
Cleboost <clement.balarot@gmail.com>
2026-06-18 19:30:17 +0200

    feat(gio): add Downloads folder icon configuration

--- .chezmoiscripts/run_once_gio-folder-icons.sh
+++ .chezmoiscripts/run_once_gio-folder-icons.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Ensure folders exist first
-mkdir -p ~/Pictures ~/Videos ~/Code ~/Projects ~/Documents
+mkdir -p ~/Pictures ~/Videos ~/Code ~/Projects ~/Documents ~/Downloads
 
 gio set ~/Pictures metadata::custom-icon "file:///usr/share/icons/WhiteSur-dark/places/scalable/folder-pictures.svg"
 gio set ~/Videos metadata::custom-icon "file:///usr/share/icons/WhiteSur-dark/places/scalable/folder-videos.svg"
 gio set ~/Documents metadata::custom-icon "file:///usr/share/icons/WhiteSur-dark/places/scalable/folder-documents.svg"
+gio set ~/Downloads metadata::custom-icon "file:///usr/share/icons/WhiteSur-dark/places/scalable/folder-download.svg"
 gio set ~/Code metadata::custom-icon "file://${HOME}/.local/share/icons/custom/folder-code.svg"
 gio set ~/Projects metadata::custom-icon "file://${HOME}/.local/share/icons/custom/folder-projects.svg"
