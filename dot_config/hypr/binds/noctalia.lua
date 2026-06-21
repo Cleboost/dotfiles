c9e516a67b1b59b0215916471db38410bb974ef9
Cleboost <clement.balarot@gmail.com>
2026-06-21 11:57:27 +0200

    fix(hypr): use kill -9 (SIGKILL) in restart shell keybind for instant restart

--- dot_config/hypr/binds/noctalia.lua
+++ dot_config/hypr/binds/noctalia.lua
@@ -25,7 +25,7 @@ hl.bind("SUPER + F5", hl.dsp.exec_cmd("noctalia msg power-cycle"))
 
 
 -- Shell Management
-hl.bind("CTRL + SUPER + ALT + R", hl.dsp.exec_cmd("pkill -6 noctalia; while pgrep -x noctalia >/dev/null; do sleep 0.1; done; noctalia"), { desc = "Restart shell" })
+hl.bind("CTRL + SUPER + ALT + R", hl.dsp.exec_cmd("pkill -9 noctalia; while pgrep -x noctalia >/dev/null; do sleep 0.1; done; noctalia"), { desc = "Restart shell" })
 hl.bind("CTRL + SHIFT + SUPER + ALT + R", hl.dsp.exec_cmd("pkill noctalia"), { desc = "Kill shell" })
 hl.bind("CTRL + SUPER + ALT + T", hl.dsp.exec_cmd("noctalia msg wallpaper-random"))
 
