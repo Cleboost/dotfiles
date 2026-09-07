02bd9e3af25d9b0061acdc3fc8cc16d3e85b229c
cleboost <clement.balarot@gmail.com>
2026-09-08 01:50:42 +0200

    fix: add backupFileExtension for home-manager collision resolution

--- flake.nix
+++ flake.nix
@@ -42,6 +42,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
           home-manager.extraSpecialArgs = { inherit inputs; };
           home-manager.users.cleboost = import ./home;
         }
