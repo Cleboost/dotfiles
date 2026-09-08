03cd49a2f4b118d36ec58cd75fe4344aa0beedf8
cleboost <clement.balarot@gmail.com>
2026-09-08 13:07:24 +0200

    feat: add chaotic-nyx binary cache substituter

--- configuration.nix
+++ configuration.nix
@@ -18,12 +18,14 @@
       "https://noctalia.cachix.org"
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
+      "https://nyx.chaotic.cx"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "chaotic-nyx.chaotic.cx-1:HfnXSw4pj75/hIxjy6Ext7524AGSTW6jZ31VC8RchNc="
     ];
   };
 
