198160fd5aa7abbf03657208739174b6b4d9c030
cleboost <clement.balarot@gmail.com>
2026-09-08 02:02:47 +0200

    fix: correct move-special.sh filename in hyprland.nix

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -13,7 +13,7 @@
   xdg.configFile."hypr/hyprland".source = ../files/hypr/hyprland;
   xdg.configFile."hypr/hyprlock".source = ../files/hypr/hyprlock;
   xdg.configFile."hypr/scripts".source = ../files/hypr/scripts;
-  xdg.configFile."hypr/move-special.sh".source = ../files/hypr/executable_move-special.sh;
+  xdg.configFile."hypr/move-special.sh".source = ../files/hypr/move-special.sh;
 
   # User custom scripts into ~/.local/bin
   home.file.".local/bin".source = ../files/bin;
