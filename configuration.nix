100e0c5aa7297e229c4955d0187f03a3ff21b3eb
cleboost <clement.balarot@gmail.com>
2026-09-08 13:06:00 +0200

    feat: add nix-community binary cache substituter

--- configuration.nix
+++ configuration.nix
@@ -17,11 +17,13 @@
       "https://cache.nixos.org"
       "https://noctalia.cachix.org"
       "https://hyprland.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
