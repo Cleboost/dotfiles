bb01dc1c89e52bcf73c3f0bef25b1b9231c479dc
cleboost <clement.balarot@gmail.com>
2026-09-08 18:40:42 +0200

    feat(fonts): add standard fonts (Roboto, Noto, Liberation, Corefonts) and set fontconfig defaults

--- modules/packages.nix
+++ modules/packages.nix
@@ -6,10 +6,27 @@
   nixpkgs.config.allowUnfree = true;
 
   # System fonts
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    font-awesome
-  ];
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      font-awesome
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+      roboto
+      liberation_ttf
+      corefonts # Microsoft TrueType core fonts (Arial, Times New Roman, etc.)
+    ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        sansSerif = [ "Roboto" "Noto Sans" "DejaVu Sans" ];
+        serif = [ "Liberation Serif" "DejaVu Serif" ];
+        monospace = [ "JetBrainsMono Nerd Font" "DejaVu Sans Mono" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
+  };
 
   # Enable Fish shell system-wide for completions and vendor functions
   programs.fish.enable = true;
