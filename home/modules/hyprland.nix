ac81c896e73606460341dc98db9f69344457238c
cleboost <clement.balarot@gmail.com>
2026-09-09 19:15:27 +0200

    fix(home-manager): force link for hyprland configs

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -2,11 +2,17 @@
 
 {
   # Link individual hypr files and folders so ~/.config/hypr remains writable (for noctalia theme generation)
-  xdg.configFile."hypr/hyprland.conf".source = ../files/hypr/hyprland.conf;
+  xdg.configFile."hypr/hyprland.conf" = {
+    source = ../files/hypr/hyprland.conf;
+    force = true;
+  };
   xdg.configFile."hypr/hypridle.conf".source = ../files/hypr/hypridle.conf;
   xdg.configFile."hypr/hyprlock.conf".source = ../files/hypr/hyprlock.conf;
   xdg.configFile."hypr/custom.conf".source = ../files/hypr/custom.conf;
-  xdg.configFile."hypr/core".source = ../files/hypr/core;
+  xdg.configFile."hypr/core" = {
+    source = ../files/hypr/core;
+    force = true;
+  };
   xdg.configFile."hypr/binds".source = ../files/hypr/binds;
   xdg.configFile."hypr/rules".source = ../files/hypr/rules;
   xdg.configFile."hypr/theme".source = ../files/hypr/theme;
